1066194fe967de4b8573acad87c25e1d9b6e482c
Maxime BAUMANN <maxime.baumann@psycle.io>
2022-07-26 10:48:00 +0200

    Add projects utils and hooks that can be public

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,18 @@
-import { useEffect, useRef, useState } from 'react'
+import { debounce, isEqual } from 'lodash'
+import {
+    DependencyList,
+    Dispatch,
+    EffectCallback,
+    MutableRefObject,
+    Ref,
+    SetStateAction,
+    useCallback,
+    useEffect,
+    useMemo,
+    useRef,
+    useState,
+} from 'react'
+import { getRandomId } from './utils'
 
 export function usePointerPosition() {
     const [position, setPosition] = useState({ clientX: 0, clientY: 0 })
@@ -54,4 +68,238 @@ export function useAnimationFrame(callback: CallableFunction) {
             requestRef.current && cancelAnimationFrame(requestRef.current)
         }
     }, [])
+}
+
+export function useCombinedRefs<TRef>(
+    ...refs: Array<Ref<TRef | null> | MutableRefObject<TRef | null>>
+) {
+    const targetRef = useRef<TRef | null>(null)
+
+    useEffect(() => {
+        refs.forEach((ref) => {
+            if (!ref) return
+
+            if (typeof ref === 'function') {
+                ref(targetRef.current)
+            } else {
+                ;(ref as MutableRefObject<TRef | null>).current =
+                    targetRef.current
+            }
+        })
+    }, [refs])
+
+    return targetRef
+}
+
+export function useFullScreen() {
+    const [isFullScreen, setIsFullScreen] = useState(
+        window.matchMedia('(display-mode: fullscreen)').matches,
+    )
+
+    const open = useCallback(() => {
+        document.documentElement.requestFullscreen()
+    }, [])
+
+    const close = useCallback(() => {
+        document.exitFullscreen()
+    }, [])
+
+    const handleChange = useCallback(() => {
+        setIsFullScreen(
+            window.matchMedia('(display-mode: fullscreen)').matches ||
+                (document.fullscreenElement !== null &&
+                    document.fullscreenElement !== undefined),
+        )
+    }, [])
+
+    useEffect(() => {
+        window.addEventListener('fullscreenchange', handleChange)
+
+        return () => {
+            window.removeEventListener('fullscreenchange', handleChange)
+        }
+    }, [])
+
+    return { isFullScreen, open, close }
+}
+
+export function useLocalStorage<T>(
+    key: string,
+    initialValue: T,
+): [T, Dispatch<SetStateAction<T>>] {
+    const getValue = useCallback(
+        (item: string | null) => {
+            if (initialValue instanceof Map) {
+                return item
+                    ? (new Map<unknown, unknown>(
+                          JSON.parse(item),
+                      ) as unknown as T)
+                    : initialValue
+            }
+
+            return item ? JSON.parse(item) : initialValue
+        },
+        [initialValue],
+    )
+
+    const [value, setValue] = useState<T>(() => {
+        const item = localStorage.getItem(key)
+
+        return getValue(item)
+    })
+
+    const onStorageUpdate = useCallback(
+        (e: StorageEvent) => {
+            if (e.key === key) {
+                const newValue = getValue(e.newValue)
+
+                if (!isEqual(newValue, value)) {
+                    setValue(newValue)
+                }
+            }
+        },
+        [getValue, key, value],
+    )
+
+    useEffect(() => {
+        window.addEventListener('storage', onStorageUpdate)
+
+        return () => {
+            window.removeEventListener('storage', onStorageUpdate)
+        }
+    }, [onStorageUpdate])
+
+    useEffect(() => {
+        if (value instanceof Map) {
+            localStorage.setItem(
+                key,
+                JSON.stringify(Array.from(value.entries())),
+            )
+        } else {
+            localStorage.setItem(key, JSON.stringify(value))
+        }
+    }, [key, value])
+
+    return [value, setValue]
+}
+
+export function usePrevious<T>(
+    value: T,
+): MutableRefObject<T | undefined>['current'] {
+    const ref = useRef<T>()
+
+    useEffect(() => {
+        ref.current = value
+    }, [value])
+
+    return ref.current
+}
+
+export function useDebounce<T>(delay: number) {
+    const [debouncedValue, setDebouncedValue] = useState<T>()
+
+    const debounceHandler = useMemo(
+        () => debounce((value: T) => setDebouncedValue(value), delay),
+        [],
+    )
+
+    useEffect(() => {
+        return () => {
+            debounceHandler.cancel()
+        }
+    }, [debounceHandler])
+
+    return [debouncedValue, debounceHandler]
+}
+
+export function useMount(callback: EffectCallback) {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(callback, [])
+}
+
+export function useUnmount(callback: EffectCallback) {
+    const callbackRef = useRef(callback)
+
+    callbackRef.current = callback
+
+    useEffect(() => {
+        return () => {
+            callbackRef.current()
+        }
+    }, [])
+}
+
+export function useUpdateEffect(
+    callback: EffectCallback,
+    deps?: DependencyList,
+) {
+    const isFirstMount = useRef(false)
+
+    useUnmount(() => {
+        isFirstMount.current = false
+    })
+
+    useEffect(() => {
+        if (isFirstMount.current) {
+            return callback()
+        } else {
+            isFirstMount.current = true
+        }
+    }, deps)
+}
+
+export function useSetState<T>(initState: T) {
+    const [state, setState] = useState<T>(initState)
+
+    const setMergeState = useCallback(
+        (value: SetStateAction<T | Partial<T>>) => {
+            setState((prevValue) => {
+                const newValue =
+                    typeof value === 'function'
+                        ? (
+                              value as (
+                                  prevState: T | Partial<T>,
+                              ) => T | Partial<T>
+                          )(prevValue)
+                        : value
+                return newValue ? { ...prevValue, ...newValue } : prevValue
+            })
+        },
+        [],
+    )
+
+    return [state, setMergeState]
+}
+
+export function useKeyPress(
+    targetKey: string,
+    handler: (event: KeyboardEvent) => void,
+) {
+    const handlerRef = useRef(handler)
+    handlerRef.current = handler
+
+    useEffect(() => {
+        const handleDown = (event: KeyboardEvent) => {
+            if (event.key === targetKey) {
+                handlerRef.current.call(window, event)
+            }
+        }
+
+        window.addEventListener('keydown', handleDown)
+
+        return () => {
+            window.removeEventListener('keydown', handleDown)
+        }
+    }, [targetKey, handler])
+}
+
+export function useId() {
+    const id = useRef(getRandomId())
+
+    return id.current
+}
+
+export function useIdOrDefault(id?: string) {
+    const defaultId = useId()
+    return id || defaultId
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,128 @@
 export const isTouchDevice =
     global.window &&
     ('ontouchstart' in global.window || navigator.maxTouchPoints > 0)
+
+export function getRandomId() {
+    const arr = new Uint32Array(1)
+    global.crypto.getRandomValues(arr)
+    return arr[0].toString(36)
+}
+
+export function getRandomNumber(min: number, max: number) {
+    const arr = new Uint32Array(1)
+    global.crypto.getRandomValues(arr)
+    const random = arr[0]
+    const range = max - min
+    return Math.floor((random / (0xffffffff + 1)) * range + min)
+}
+
+export function groupBy<T extends Record<string | number, string | number>>(
+    arr: T[],
+    key: string | number,
+) {
+    return arr.reduce((acc, cur) => {
+        const value = acc.get(cur[key])
+
+        if (!value) {
+            acc.set(cur[key], [cur])
+        } else {
+            acc.set(cur[key], [...value, cur])
+        }
+
+        return acc
+    }, new Map<string | number, Array<T>>())
+}
+
+export function isReactMouseEvent<TElement = Element>(
+    event: React.SyntheticEvent<TElement, MouseEvent | KeyboardEvent>,
+): event is React.SyntheticEvent<TElement, MouseEvent> {
+    return event.nativeEvent instanceof MouseEvent
+}
+
+export function isReactKeyboardEvent<TElement = Element>(
+    event: React.SyntheticEvent<TElement, MouseEvent | KeyboardEvent>,
+): event is React.SyntheticEvent<TElement, KeyboardEvent> {
+    return event.nativeEvent instanceof KeyboardEvent
+}
+
+export function numberToHex(number: number): string {
+    const hex = number.toString(16)
+    return hex.length === 1 ? `0${hex}` : hex
+}
+
+export function bgrToHex(b: number, g: number, r: number) {
+    return `#${numberToHex(r)}${numberToHex(g)}${numberToHex(b)}`
+}
+
+export function hexToBgr(hex: string): [number, number, number] | null {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+    return result
+        ? [
+              parseInt(result[3], 16),
+              parseInt(result[2], 16),
+              parseInt(result[1], 16),
+          ]
+        : null
+}
+
+export async function copyToClipboard(text: string) {
+    if (!navigator.clipboard) {
+        const textarea = document.createElement('textarea')
+        textarea.value = text
+        textarea.setAttribute('readonly', '')
+        textarea.style.position = 'fixed'
+        textarea.style.top = '0'
+        textarea.style.left = '0'
+        document.body.appendChild(textarea)
+
+        textarea.focus()
+        textarea.select()
+
+        try {
+            const success = document.execCommand('copy')
+
+            if (!success) {
+                throw new Error('unable to copy')
+            }
+        } finally {
+            document.body.removeChild(textarea)
+        }
+
+        return
+    }
+
+    return await navigator.clipboard.writeText(text)
+}
+
+const ipv4Regex = new RegExp(
+    '^(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)(?:\\.(?:25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)){3}$',
+)
+export function isIPV4(ip: string) {
+    return ipv4Regex.test(ip)
+}
+
+export function downloadFile(blob: Blob, filename: string) {
+    const url = window.URL.createObjectURL(blob)
+    const link = document.createElement('a')
+
+    link.href = url
+    link.setAttribute('download', filename)
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+}
+
+export function isNumberKeyEvent(event: KeyboardEvent): number | false {
+    const key = event.key
+
+    const number = parseInt(key, 10)
+    if (number >= 0 && number <= 9) {
+        return number
+    }
+
+    return false
+}
+
+export function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
